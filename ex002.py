8c3657b8151a5f478aac2351c1ba596d077d2cf9
JamilyGSS <salesjamily0@gmail.com>
2025-05-03 09:33:08 -0300

    ex002

---
+++ ex002.py
@@ -0,0 +1,7 @@
+x = 2
+y = 2.556
+nome = "Roberto"
+
+print(type(x))
+print(type(y))
+print(type(nome))
---
+++ ex003.py
@@ -0,0 +1,6 @@
+nome = "Giovana"
+idade = 17
+cpf = "33333333333"
+print("Seu nome é",nome)
+print("Você tem ", idade,"anos")
+print("Seu CPF é", cpf)
