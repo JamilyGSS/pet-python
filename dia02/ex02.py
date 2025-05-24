2fb0960865b8f795ef6fb928466b26c54f455c3a
JamilyGSS <salesjamily0@gmail.com>
2025-05-24 09:44:48 -0300

    Update ex02.py

---
+++ dia02/ex02.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+n3 = int(input("Digite o terceiro número: "))
+I = [n1,n2,n3]
+soma = I[0] + I[1] + I[2]
+print(I)
+print(f'A soma dos valores é {soma}')
