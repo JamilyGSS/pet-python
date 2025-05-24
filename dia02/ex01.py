db5d04d7a6393081fbb93e66538ec40932ae8a58
JamilyGSS <salesjamily0@gmail.com>
2025-05-24 08:50:46 -0300

    Create ex01.py

---
+++ dia02/ex01.py
@@ -0,0 +1,7 @@
+ra = input('Digite seu ra: ')
+email = "ra"+ra+"@uem.br"
+print(email)
+
+c = float(input("Digite a temperatura em Celsius: "))
+f = (c * 9/5) + 32 
+print(f"A temperatura em Farenheit é {f}")
