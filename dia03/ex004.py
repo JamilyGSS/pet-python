1f844a60354c7ba110a903c5ebcb8281ca47070d
JamilyGSS <salesjamily0@gmail.com>
2025-05-31 11:13:57 -0300

    ex004

---
+++ dia03/ex004.py
@@ -0,0 +1,9 @@
+def saudacao(nome:str):
+    '''Mostra uma saudação a partir do *nome*.
+    Exemplo:
+    >>> saudacao('Gigi')
+    Bom dia Gigi, como está?
+    '''
+    print(f'Bom dia {nome}, como está?')
+
+saudacao('Ana')
