84294bdfc4c514ce44b6a978fcfd2a5414f3e76a
JamilyGSS <salesjamily0@gmail.com>
2025-06-07 09:11:33 -0300

    media

--- dia04/ex001.py
+++ dia04/ex001.py
@@ -1,3 +1,3 @@
 estados = {"Pernambuco":"Recife", "Paraná":"Curitiba","Paraiba": "João Pessoa","Rio Grande do Norte": "Natal","Ceará":"Fortaleza"}
-for elementos in estados.items():
-    print(elementos)
+for chave, valor in estados.items():
+    print(f'A capital de {chave} é {valor}')
