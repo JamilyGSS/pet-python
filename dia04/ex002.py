a85d423bdb8593132b0a0707cebcee0621a2ee53
JamilyGSS <salesjamily0@gmail.com>
2025-06-07 09:13:51 -0300

    Update ex002.py

--- dia04/ex002.py
+++ dia04/ex002.py
@@ -1,8 +1,9 @@
 notas = {'Leandro': [0.2,4.6,9], 'Bernardo':[4.9,8,4.7] , 'Lucas':[7,8.9,2]}
 
-for nome, media in notas.items():
-    print(f'Média de {nome}: {sum(notas[nome])/3:.2f}')
-    if sum(notas[nome])/3 < 6:
+for nome, notas in notas.items():
+    media = sum(notas)/3
+    print(f'Média de {nome}: {media:.2f}')
+    if media < 6:
         print('Aluno REPROVADO')
     else:
         print('Aluno APROVADO')
