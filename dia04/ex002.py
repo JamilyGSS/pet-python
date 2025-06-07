67e653eae9e557d35f4d2a629e31eb636cc778ac
JamilyGSS <salesjamily0@gmail.com>
2025-06-07 09:12:29 -0300

    Update ex002.py

--- dia04/ex002.py
+++ dia04/ex002.py
@@ -1,11 +1,5 @@
 notas = {'Leandro': [0.2,4.6,9], 'Bernardo':[4.9,8,4.7] , 'Lucas':[7,8.9,2]}
 
-def sum(lista: list[float]) -> float:
-    soma = 0
-    for num in lista:
-        soma += num
-    return soma
-
 for nome, media in notas.items():
     print(f'Média de {nome}: {sum(notas[nome])/3:.2f}')
     if sum(notas[nome])/3 < 6:
