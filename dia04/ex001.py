331dd444f1a5a1a0cce06faa81f42bf6bc151312
JamilyGSS <salesjamily0@gmail.com>
2025-06-07 08:45:24 -0300

    Create ex001.py

---
+++ dia04/ex001.py
@@ -0,0 +1,3 @@
+estados = {"Pernambuco":"Recife", "Paraná":"Curitiba","Paraiba": "João Pessoa","Rio Grande do Norte": "Natal","Ceará":"Fortaleza"}
+for elementos in estados.items():
+    print(elementos)
