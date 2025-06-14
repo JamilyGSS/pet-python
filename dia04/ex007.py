eb00d829dfcdc7a909d33c86b4423937ffc453b2
JamilyGSS <salesjamily0@gmail.com>
2025-06-14 13:40:53 -0300

    Update ex007.py

--- dia04/ex007.py
+++ dia04/ex007.py
@@ -21,9 +21,24 @@ def adicionar_item(lista1: list) -> list:
     lista_compras[novo_item] = quant_novo_item
     return lista_compras
 
+def remover_item(lista1: list) -> list:
+    item_excluido = input('Digite o item que você quer excluir: ')
+    teste = True
+    while teste:
+        if item_excluido not in lista_compras:
+            print('O item não está na lista! Tente novamente!')
+            item_excluido = input('Digite o novo item: ')
+        else:
+            teste = False
+    print()
+    lista_compras[novo_item] = quant_novo_item
+    return lista_compras
+
 res = 0
 while res != 5:
     res = int(input('O que você quer fazer? \n[1] Adicionar Novo Item \n[2] Remover um Item \n[3] Atualizar a Quantidade de um Item \n[4] Visualizar a lista \n[5] Sair do Programa \n'))
     verificar_res(res)
     if res == 1:
         adicionar_item(lista_compras)
+    elif res == 2:
+        remover_item(lista_compras)
